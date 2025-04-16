467afa1fceee0fe0167aaab7c645fae86b876134
Gustavo Ferreira <gugacabral03@gmail.com>
2025-04-16 11:40:41 -0300

    Remover pasta dist/ do versionamento

--- dist/routes/router.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const example_controller_1 = require("../controllers/example.controller");
-const router = (0, express_1.Router)();
-router.use("/getexemple", example_controller_1.getAllExamples);
-router.use("/postExemple", example_controller_1.createExample);
-exports.default = router;
